eb98bd379d1adbd4e0cd17172910904eea3bce61
SofiaMHallberg <62309798+SofiaMHallberg@users.noreply.github.com>
2021-02-19 13:50:55 +0100

    Färdig

--- SysII-Labb-Testning-Part-One/test/java/main/RovLanguageTest.java
+++ SysII-Labb-Testning-Part-One/test/java/main/RovLanguageTest.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 
@@ -16,24 +17,62 @@ public class RovLanguageTest {
     }
 
     @Test
-    void enRov() {
+    @DisplayName("Testing null")
+    void enRovNull() {
         Assert.assertNull(RovLanguage.enRovLang(null));
+           }
+
+    @Test
+    @DisplayName("Testing small letters")
+    void enRovSmall() {
         Assert.assertEquals("abobcocdodefofgoghohijojkoklolmomnonopopqoqrorsostotuvovwowxoxyzozåäö", RovLanguage.enRovLang("abcdefghijklmnopqrstuvwxyzåäö"));
+    }
+
+    @Test
+    @DisplayName("Testing big letters")
+    void enRovBig() {
         Assert.assertEquals("ABOBCOCDODEFOFGOGHOHIJOJKOKLOLMOMNONOPOPQOQRORSOSTOTUVOVWOWXOXYZOZÅÄÖ", RovLanguage.enRovLang("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"));
+    }
+
+    @Test
+    @DisplayName("Testing signs")
+    void enRovSigns() {
         Assert.assertEquals("!#€%&/()=?@0123456789",RovLanguage.enRovLang("!#€%&/()=?@0123456789"));
+    }
+
+    @Test
+    @DisplayName("Testing empty string")
+    void enRovEmptyString() {
         Assert.assertEquals("",RovLanguage.enRovLang(""));
     }
 
-    //ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ
-    //abcdefghijklmnopqrstuvwxyzåäö
+    @Test
+    @DisplayName("Testing null")
+    void deRovNull() {
+        Assert.assertNull(RovLanguage.deRovLang(null));
+    }
 
     @Test
-    void deRov() {
-        Assert.assertNull(RovLanguage.deRovLang(null));
+    @DisplayName("Testing small letters")
+    void deRovSmall() {
         Assert.assertEquals(RovLanguage.deRovLang("abobcocdodefofgoghohijojkoklolmomnonopopqoqrorsostotuvovwowxoxyzozåäö"),"abcdefghijklmnopqrstuvwxyzåäö");
-        Assert.assertEquals(RovLanguage.deRovLang("ABOBCOCDODEFOFGOGHOHIJOJKOKLOLMOMNONOPOPQOQRORSOSTOTUVOVWOWXOXYZOZÅÄÖ"),"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ");
-        Assert.assertEquals(RovLanguage.enRovLang("!#€%&/()=?@0123456789"),"!#€%&/()=?@0123456789");
-        Assert.assertEquals("",RovLanguage.enRovLang(""));
+    }
 
+    @Test
+    @DisplayName("Testing big")
+    void deRovBig() {
+        Assert.assertEquals(RovLanguage.deRovLang("ABOBCOCDODEFOFGOGHOHIJOJKOKLOLMOMNONOPOPQOQRORSOSTOTUVOVWOWXOXYZOZÅÄÖ"),"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ");
+    }
+
+    @Test
+    @DisplayName("Testing signs")
+    void deRovSigns() {
+        Assert.assertEquals(RovLanguage.enRovLang("!#€%&/()=?@0123456789"),"!#€%&/()=?@0123456789");
+    }
+
+    @Test
+    @DisplayName("Testing empty string")
+    void deRov() {
+        Assert.assertEquals("",RovLanguage.enRovLang(""));
     }
 }
